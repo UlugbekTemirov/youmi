919324c31ae17a4bc024cdee741e532d0583831a
justmirshod <98507846+justmirshod@users.noreply.github.com>
2022-12-17 10:07:37 +0500

    modified

--- src/pages/Blog/components/Categories/Categories.js
+++ src/pages/Blog/components/Categories/Categories.js
@@ -3,39 +3,48 @@ import apathy from "../../../../assets/images/apathy 1.svg";
 import depression from "../../../../assets/images/depression 1.svg";
 import emotions from "../../../../assets/images/emotions 1.svg";
 import partners from "../../../../assets/images/partners 1.svg";
+import { Link } from "react-router-dom";
 
 //categories list
 const categoriesList = [
   {
     name: "Отношения с партнером",
+    eng: "relationship-between-partners",
     img: partners,
   },
   {
     name: "Эмоции",
+    eng: "emotions",
     img: emotions,
   },
   {
     name: "Чувство тревоги",
+    eng: "sense-of-anxiety",
     img: apathy,
   },
   {
     name: "Депрессивное состояние",
+    eng: "depressive-state",
     img: depression,
   },
   {
     name: "Отношения с партнером",
+    eng: "relationship-between-partners",
     img: partners,
   },
   {
     name: "Эмоции",
+    eng: "emotions",
     img: emotions,
   },
   {
     name: "Чувство тревоги",
+    eng: "sense-of-anxiety",
     img: apathy,
   },
   {
     name: "Депрессивное состояние",
+    eng: "depressive-state",
     img: depression,
   },
 ];
@@ -46,7 +55,8 @@ const Categories = () => {
       <Container>
         <div className="grid grid-cols-4 gap-6">
           {categoriesList.map((item, index) => (
-            <div
+            <Link
+              to={`/blog/category/${item.eng}`}
               key={index}
               className="col-span-1 rounded-[20px] bg-aqua p-4 flex flex-col justify-between cursor-pointer"
             >
@@ -54,7 +64,7 @@ const Categories = () => {
               <div className="flex justify-end w-full ">
                 <img src={item.img} alt="" />
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </Container>
