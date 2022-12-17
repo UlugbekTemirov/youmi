dda000c2d283a3a8eda3dc1f75bf7bbe02ba987d
justmirshod <98507846+justmirshod@users.noreply.github.com>
2022-12-17 11:59:32 +0500

    `changed hugphone`

--- src/components/HugPhone/HugPhone.js
+++ src/components/HugPhone/HugPhone.js
@@ -6,7 +6,7 @@ const HugPhone = () => {
   return (
     <div className=" mt-40">
       <Container>
-        <div className="bg-aqua-light grid grid-cols-2 box-border px-[120px] py-[80px] max-h-[400px]">
+        <div className="bg-aqua-light grid grid-cols-2 box-border px-[120px] py-[80px] max-h-[400px] rounded-[20px]">
           <div className="col-span-1">
             <h1 className="text-purple text-40 font-extrabold mb-10">
               Подберите своего психолога онлайн
