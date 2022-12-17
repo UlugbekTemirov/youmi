97a4cac1cc97da947ef07e0cb4187f8d451ae7e7
justmirshod <98507846+justmirshod@users.noreply.github.com>
2022-12-17 10:10:42 +0500

    added new exactpath slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navbar from "../components/Navbar/navbar_slice";
+import exactpath from "../components/ExactPath/exactpath_slice";
 
 export const store = configureStore({
-  reducer: { navbar },
+  reducer: { navbar, exactpath },
   devTools: process.env.NODE_ENV !== "production",
 });
